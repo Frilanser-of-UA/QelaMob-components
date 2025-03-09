6388a06ec903373236ae13b40572feef03a9a8fb
“Andrey <foresteraws@gmail.com>
2025-03-09 22:08:28 +0200

    Add new components chat + chats page

--- js/app.js
+++ js/app.js
@@ -1,34 +1,11 @@
+const btnChatBottom = document.querySelector('.qela-chat-btn');
+const navBottom = document.querySelector('.qela-bottom')
+const observer = new ResizeObserver(entrirs => {
+	const elem = entrirs[0];
+	console.log(elem);
+	let heightAside = elem.borderBoxSize[0].blockSize;
+	btnChatBottom.style.bottom = heightAside + 'px';
+	console.log(btnChatBottom.style.bottom = heightAside + 'px');
 
-window.onload = function () {
-	document.addEventListener("click", documentActions);
-	// Actions ( делегирование собитий click)
-	function documentActions(e) {
-		const targetElement = e.target;
-		// ====== Меню
-		if (targetElement.classList.contains('qela-btn-menu') || targetElement.classList.contains('qela-burger')) {
-			if (document.querySelector('.qela-btn-menu') || document.querySelector('.qela-burger')) {
-				document.querySelector('.qela-menu').classList.toggle('active');
-				document.querySelector('.qela-btn-menu').classList.toggle('active');
-			}
-		} else if (!targetElement.closest('#gela-nav') && document.querySelector('.qela-menu.active')) {
-			document.querySelector('.qela-menu').classList.remove('active');
-			document.querySelector('.qela-btn-menu').classList.remove('active');
-		}
-		//== випадашки search
-		if (targetElement.classList.contains('qela-search-btn')) {
-			if (document.querySelector('.qela-search-btn')) {
-				document.querySelector('.qela-wrapper').classList.toggle('qela--visible');
-			}
-		} else if (!targetElement.closest('.qela-wrapper') && document.querySelector('.qela-wrapper.qela--visible')) {
-			document.querySelector('.qela-wrapper').classList.remove('qela--visible');
-		}
-		//== випадашки dots
-		if (targetElement.classList.contains('dots-table')) {
-			if (document.querySelector('.dots-table')) {
-				document.querySelector('.dots-table').classList.toggle('active');
-			}
-		} else if (!targetElement.closest('.dots-table') && document.querySelector('.dots-table.active')) {
-			document.querySelector('.dots-table').classList.remove('active');
-		}
-	}
-}
+})
+observer.observe(navBottom);
